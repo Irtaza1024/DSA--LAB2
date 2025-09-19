ddbea2da05e58c15f5c5883fbf0da0733eafb18c
Muhammad Irtaza <mjawad.bese24seecs@seecs.edu.pk>
2025-09-19 04:04:57 -0700

    Implement an analyze pointer function

---
+++ analyze_pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void analyze_pointer(int * ptr){
+	cout << ptr << endl;
+	cout << *ptr << endl;
+}
+
+int main(){
+	int iValue = 10;
+	analyze_pointer(&iValue);
+
+	int * jValue = new int(20);
+	analyze_pointer(jValue);
+}
